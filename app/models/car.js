9a8544edb8a7b2550b422ad8a1be6e37c57006a0
ardhimas <kamdani.af@gmail.com>
2016-03-25 22:35:04 +0000

    started router and schema

--- app/models/car.js
+++ app/models/car.js
@@ -1,9 +1,12 @@
 // app/models/car.js
 // grab the mongoose module
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // define our car model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Car', {
-    name : {type : String, default: ''}
+var CarSchema = new Schema({
+    name: String
 });
+
+module.exports = mongoose.model('Car', CarSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var Car            = require('./app/models/car')
 
 // configuration ===========================================
     
@@ -37,6 +38,54 @@ app.use(express.static(__dirname + '/public'));
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
 
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/cars')
+
+    // create a car (accessed at POST http://localhost:8080/api/cars)
+    .post(function(req, res) {
+        
+        var car = new Car();      // create a new instance of the Car model
+        car.name = req.body.name;  // set the cars name (comes from the request)
+
+        // save the car and check for errors
+        car.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Car created!' });
+        });
+    })
+
+    // get all the cars (accessed at GET http://localhost:8080/api/cars)
+    .get(function(req, res) {
+        Car.find(function(err, cars) {
+            if (err)
+                res.send(err);
+
+            res.json(cars);
+        });
+    });
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
 // start app ===============================================
 // startup our app at http://localhost:8080
 app.listen(port);               
@@ -46,3 +95,4 @@ console.log('Magic happens on port ' + port);
 
 // expose app           
 exports = module.exports = app;
+
