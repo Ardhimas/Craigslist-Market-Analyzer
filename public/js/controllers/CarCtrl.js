a363a45e15c0c526ed407f8b7e48c6776c58d7db
ardhimas <kamdani.af@gmail.com>
2016-03-28 16:50:38 +0000

    successfully used API in controller to retrieve data

--- public/js/controllers/CarCtrl.js
+++ public/js/controllers/CarCtrl.js
@@ -1,6 +1,9 @@
 // public/js/controllers/CarCtrl.js
-angular.module('CarCtrl', []).controller('CarController', function($scope) {
+angular.module('CarCtrl', []).controller('CarController', function($scope, Car) {
 
     $scope.tagline = 'Nothing beats a pocket protector!';
+    Car.getAll().success(function(data){
+        $scope.carData = data;
+    });
 
 });
--- public/js/services/CarService.js
+++ public/js/services/CarService.js
@@ -3,11 +3,17 @@ angular.module('CarService', []).factory('Car', ['$http', function($http) {
 
     return {
         // call to get all cars
-        get : function() {
+        getAll : function() {
+            console.log($http.get('/api/cars'));
             return $http.get('/api/cars');
         },
 
-
+        // call to get car by ID
+        get : function(id) {
+            // var log = $http.get('/api/cars/' + id);
+            // console.log(log);
+            return $http.get('/api/cars/' + id);
+        },
                 // these will work when more API routes are defined on the Node side of things
         // call to POST and create a new car
         create : function(carData) {
