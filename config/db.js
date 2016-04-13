ef9da8d4ec22efc9960df68ea85c548c47e1f18b
ardhimas <kamdani.af@gmail.com>
2016-04-13 22:51:14 +0000

    changed db url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://ardhimas-craigslist-market-analyzer-2804007:27017'
+        // url : 'mongodb://ardhimas-craigslist-market-analyzer-2804007:27017'
+    
+        url: 'mongodb://ardy:iamthemaster@ds025180.mlab.com:25180/car-data'
     }
