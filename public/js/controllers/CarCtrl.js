ea7b78ef4f1910bc26c171557db63f2870cc67fc
ardhimas <kamdani.af@gmail.com>
2016-04-25 23:47:55 +0000

    adding pagination for tables

--- public/js/controllers/CarCtrl.js
+++ public/js/controllers/CarCtrl.js
@@ -1,7 +1,20 @@
 // public/js/controllers/CarCtrl.js
-angular.module('CarCtrl', []).controller('CarController', function($scope, Car) {
+angular.module('CarCtrl', []).controller('CarController', function($scope, $rootScope, Car) {
 
     $scope.tagline = 'Data retrieved from MongoDB database hosted on mLab';
+    
+    
+      
+  $scope.paginate = function(value) {
+    $scope.totalItems = $scope.carData.length;
+    $scope.currentPage = 1;
+    $scope.numPerPage = 10;
+    var begin, end, index;
+    begin = ($scope.currentPage - 1) * $scope.numPerPage;
+    end = begin + $scope.numPerPage;
+    index = $scope.carData.indexOf(value);
+    return (begin <= index && index < end);
+  };
     $scope.removeCar = function(car) {
         $scope.carData.splice($scope.carData.indexOf(car), 1);
         Car.delete(car.carID);
